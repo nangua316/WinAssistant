10354beb3b3b715a4d205968c0f8b697bddd7a00
Developer <developer@example.com>
2026-05-28 14:43:36 +0800

    release: v2026.5.28.1

---
+++ WinAssistant/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WinAssistant")]
+[assembly: AssemblyDescription("Windows 效率工具合集")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WinAssistant")]
+[assembly: AssemblyCopyright("Copyright © 2026")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("2026.5.28.1")]
+[assembly: AssemblyFileVersion("2026.5.28.1")]
+[assembly: AssemblyInformationalVersion("2026.5.28.1")]
